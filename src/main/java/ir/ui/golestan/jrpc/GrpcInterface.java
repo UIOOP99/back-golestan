3922836c212c5d86469d9e69cafeda172cbdfcfe
Sepehr <s.baneshi98@gmail.com>
2020-11-10 11:16:40 +0330

    grpc interface and test implementation added

---
+++ src/main/java/ir/ui/golestan/jrpc/GrpcInterface.java
@@ -0,0 +1,4 @@
+package ir.ui.golestan.jrpc;
+
+public interface GrpcInterface {
+}
---
+++ src/main/java/ir/ui/golestan/jrpc/GrpcTestInterface.java
@@ -0,0 +1,7 @@
+package ir.ui.golestan.jrpc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GrpcTestInterface implements GrpcInterface {
+}
