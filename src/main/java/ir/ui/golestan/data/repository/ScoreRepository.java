01ed7dd7beb465d7d600828687a225f6e2589ac0
sbaneshi <60042810+sbaneshi@users.noreply.github.com>
2020-12-05 00:18:27 +0330

    Apply suggestions from code review
    Co-authored-by: AhmadMahmoodianDarvishani <39862634+ahmadmahmoodiandarvishani@users.noreply.github.com>

--- src/main/java/ir/ui/golestan/data/repository/ScoreRepository.java
+++ src/main/java/ir/ui/golestan/data/repository/ScoreRepository.java
@@ -10,6 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Score.ScoreId> {
 
-    List<Score> findScoresBystudentId(int studentId);
+    List<Score> findAllByStudentId(int studentId);
 }
-
