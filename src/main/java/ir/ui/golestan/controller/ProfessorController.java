e3595748cacdadb5215e19974dd6b0f90bec088e
Sepehr <s.baneshi98@gmail.com>
2021-01-22 19:20:12 +0330

    add some reqs and release version 1.0

--- src/main/java/ir/ui/golestan/controller/ProfessorController.java
+++ src/main/java/ir/ui/golestan/controller/ProfessorController.java
@@ -9,23 +9,30 @@ import ir.ui.golestan.data.entity.Course;
 import ir.ui.golestan.data.entity.Score;
 import ir.ui.golestan.data.repository.CourseRepository;
 import ir.ui.golestan.data.repository.ScoreRepository;
+import ir.ui.golestan.grpc.AuthGrpcClientService;
 import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProfessorController extends BaseController {
 
     private final ScoreRepository scoreRepository;
     private final CourseRepository courseRepository;
+    private final AuthGrpcClientService authGrpcClientService;
 
     public ProfessorController(GolestanConfiguration configuration, AuthorizationService authorizationService,
-                               ScoreRepository scoreRepository, CourseRepository courseRepository) {
+                               ScoreRepository scoreRepository, CourseRepository courseRepository, AuthGrpcClientService authGrpcClientService) {
         super(configuration, authorizationService);
         this.scoreRepository = scoreRepository;
         this.courseRepository = courseRepository;
+        this.authGrpcClientService = authGrpcClientService;
     }
 
     @GetMapping("/professor/set_score")
@@ -44,5 +51,19 @@ public class ProfessorController extends BaseController {
     public List<Course> getAllSemesterCourses(RequestEntity<?> request, int semesterId) {
         AuthenticatedUser user = getAuthenticatedUser(request, Role.PROFESSOR);
         return courseRepository.findAllByProfessorIdAndSemesterId(user.getUserId(), semesterId);
+    }
+
+    @GetMapping("/professor/get_course_students")
+    public List<Map<String, Object>> getCourseStudents(int courseId) {
+        return Arrays.stream(courseRepository.getOne(courseId).getStudentsIds())
+                .mapToObj(studentId -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("first_name", "ATAL");
+                    map.put("last_name", "MATAL");
+                    map.put("username", "TOTOLE");
+
+                    map.put("score", scoreRepository.getOne(Score.ScoreId.of(studentId, courseId)).getScore());
+                    return map;
+                }).collect(Collectors.toList());
     }
 }
